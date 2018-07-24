9c8f27ef8443cd97aac08c76e55c649be4fc6a14
sbn <sbn@.com>
2018-07-24 12:31:06 +0800

    基本OK

---
+++ Heart wall/js/top.js
@@ -0,0 +1,37 @@
+$(function () {
+    //App下载鼠标 显示二维码
+    $('.first').mouseenter(function () {
+        $(this).children('img').stop().slideDown();
+    })
+    $('.first').mouseleave(function () {
+        $(this).children('img').stop().slideUp();
+    })
+    //设置slider的背景图
+    var $lisSli = $('.slider  li');
+    for (var i = 0; i < $lisSli.length; i++) {
+        $($lisSli[i]).css({
+            backgroundImage: 'url(./images/' + (i + 1) + '.jpg)',
+            height: 480,
+        })
+    }
+    //地区选择 
+    $('.area-choose').mouseover(function () {
+        $(this).find('ul').stop().slideDown();
+    })
+    $('.area-choose').mouseout(function () {
+        $(this).find('ul').stop().slideUp();
+    })
+    //area-choose的替换 
+    // var $lis1 = $('.area-choose li');
+    // console.log($lis1)
+    // $lis1.each(function(i,ele){
+    //     ele.click(function(){
+    //         console.log('aaa')
+    //          $('.area-choose span').text($(this).children().text());
+    //     })
+    // })
+    var location=$('.area-choose span'),lis=$('.area-choose li');
+    $('.area-choose ul').on('click','li',function(){
+        location.text($(this).children().text());
+    })
+})
--- Heart wall/js/xydos.js
+++ Heart wall/js/xydos.js
@@ -9,8 +9,8 @@ $(function () {
 
 
     var data = [
-        {id:1,xysj: "2018-7-19 22:27:2", xy: "希望明天捡十块钱！", xb: "女", name:"见钱眼开", jssj:1533105211082},
-        {id:2,xysj: "2018-7-19 22:29:5", xy: "希望明天死情敌！", xb: "女", name:"死了都要爱", jssj:1533511211082},
+        {id:1,xysj: "2018-7-19 22:27:2", xy: "希望明天捡十块钱！", name:"见钱眼开", jssj:1533105211082},
+        {id:2,xysj: "2018-7-19 22:29:5", xy: "希望明天死情敌！", name:"死了都要爱", jssj:1533511211082},
         {id:3,xysj: "2016-02-17 01:01:01", xy: "今天你拿苹果支付了么", name: "mahu", jssj:1533155211082},
         {id:4,xysj: "2018-7-20 20:42:25", xy: "希望能找到工作", name: "梦游西天", jssj:1543103211082},
         {id:5,xysj: "2018-7-20 20:45:19", xy: "我要500万", name: "钱眼子", jssj:1534100211082}
@@ -20,7 +20,7 @@ $(function () {
     var ZIndex = 1;
     var mIndex = 3;
     // ------------------------------------------------------
-
+    cjfun(data);
     // 添加心愿按钮
     $('#btn').click(function () {
         if ($(this).html() === '添加心愿') {
@@ -42,19 +42,22 @@ $(function () {
     });
 
     // 设置是否保密类名切换
-    $('.privary>input').click(function () {
+    $('.privary>input').click(function (e) {
 
         $(this).stop().toggleClass("clor").siblings().removeClass('clor');
 
         // 判断是否需要保密心愿
         if ($(this).attr('tada-n') === 'y' && $(this).attr('class') === 'check clor') {
-
+            $('html').css('overflow','hidden')
+                // $('body').css();//浮层出现时窗口不能滚动设置
+                // 
+            
             // 设置保密密码；
             $('.mask').show().css({
                     'zIndex': mIndex,
-                    height: $('#content')[0].offsetHeight,
+                    height: $('#content')[0].offsetHeight+100,
                 })
-
+                
                 //输入密码时的逻辑 
                 .find('.prow').keyup(function () {
                     var str = $('.prow').val();
@@ -75,10 +78,15 @@ $(function () {
                 }).parent('.title').children('.Ximg').click(function () {
                     $('.mask').hide();
                     $('.privary').children('input').removeClass('clor');
+                    $('html').css('overflow','auto');
                 }).parent('.title').children('.shuell').click(function () {
                     $('.mask').hide();
+
+                    // 回复页面的滚动效果
+                    $('html').css('overflow','auto');
                 })
             // ------------------------------------
+            
             // 动态显示密码 1秒隐藏
             function hidspan() {
                 var str = $('.prow').val();
@@ -128,6 +136,7 @@ $(function () {
         if (text !== '') {
             var priv = $('.privary .clor').attr('tada-n');
             var name = $('.names').val();
+            var arr = [];
             var json = {};
             var bmxysz = [];
             if (priv === 'y') {
@@ -146,112 +155,94 @@ $(function () {
                     'jssj': endx
                 }
             }
-
             data[data.length] = json;
-            console.log(data,json);
-            // 根据客户动态输入的数据创建心愿卡
-            $tip1.clone(true).show().appendTo($cont)
-                .find('.num').html('第[' + json.id + ']条' + json.xysj)
-                .parents('.tip1').find('.tip_c').text(json.xy)
-                .parents('.tip1').find('.name').text(json.name)
-                .parents('.tip1').css({
-                    left: $tip1[0].offsetLeft + Math
-                        .random() * ($cont[0].offsetWidth -
-                            $tip1[0].offsetWidth - $tip1[0].offsetLeft),
-                    top: Math.random() * 400
-                })
-                .parents('.tip1').show();
-
-            // 获取倒计时的span标签
-            // var $ele = $('#content').find('.dtime').eq(0);
-
-            // // 问题倒计时为什么不动？？？？？？？？？？？？？？？？？？？？
-            // $ele.timer = setInterval(function () {
-            //     $ele.html(fndjs(endx));
-            // }, 1000);
+            arr[arr.length] = json;
+            cjfun(arr);
+        }else{
+            alert('请输入心愿内容！');
         }
         
         $tip1.hide();
     });
     // ----------------------------------------
-
+    
     // 根据data数组里的每一项创建心愿卡
-    $.each(data, function (i, ele) {
-
+    function cjfun(arr){
+        
         //结束时间的毫秒值在data数组中获取的
-        var ends = ele.jssj;
-        $tip1.clone(true).appendTo($cont)
-            .find('.num').html('第[' + ele.id + ']条' + ele.xysj)
-            .parents('.tip1').find('.tip_c').text(ele.xy)
-
-            .parents('.tip1').find('.name').text(ele.name)
-            .parents('.tip1').css({
-                left: $tip1[0].offsetLeft + Math.random() * ($cont[0].offsetWidth -
-                    $tip1[0].offsetWidth - $tip1[0].offsetLeft),
-                top: Math.random() * 400
-            })
-            .find('.dtime').html('djsflf')
-
-            // on()：jquery事件监听器
-            .parents('#content').on('mousedown', '.tip1', function (e) {
-                e = e || document.event;
-                $(this).css('zIndex', ZIndex);
-
-                // mIndex设置遮罩层的层级
-                mIndex = 2 + ZIndex++;
-                var x = e.clientX - this.offsetLeft;
-                var y = e.clientY - this.offsetTop;
-                $(this).bind('mousemove', function (e) {
-                        e = e || document.event;
-                        var xx = e.clientX - x;
-                        var yy = e.clientY - y;
-                        var num = content.offsetWidth - this.offsetWidth;
-                        xx = xx > 0 ? xx : 0;
-                        xx = xx < num + 5 ? xx : num + 5;
-                        yy = yy > 0 ? yy : 0;
-                        yy = yy < content.offsetHeight - this.offsetHeight ?
-                            yy : content.offsetHeight - this.offsetHeight;
-                        $(this).css({
-                            left: xx,
-                            top: yy
+        $.each(arr, function (i, ele) {
+            var ends = ele.jssj;
+            $tip1.clone(true).insertBefore($tip1)
+                .find('.num').html('第[' + ele.id + ']条' + ele.xysj)
+                .parents('.tip1').find('.tip_c').text(ele.xy)
+                .parents('.tip1').find('.name').text(ele.name)
+                .parents('.tip1').css({
+                    left: $tip1[0].offsetLeft + Math.random() * ($cont[0].offsetWidth -
+                        $tip1[0].offsetWidth - $tip1[0].offsetLeft),
+                    top: Math.random() * 400
+                }).show()
+    
+                // on()：jquery事件监听器
+                .parents('#content').on('mousedown', '.tip1', function (e) {
+                    e = e || document.event;
+                    $(this).css('zIndex', ZIndex);
+    
+                    // mIndex设置遮罩层的层级
+                    mIndex = 2 + ZIndex++;
+                    var x = e.clientX - this.offsetLeft;
+                    var y = e.clientY - this.offsetTop;
+                    $(this).bind('mousemove', function (e) {
+                            e = e || document.event;
+                            var xx = e.clientX - x;
+                            var yy = e.clientY - y;
+                            var num = content.offsetWidth - this.offsetWidth;
+                            xx = xx > 0 ? xx : 0;
+                            xx = xx < num + 5 ? xx : num + 5;
+                            yy = yy > 0 ? yy : 0;
+                            yy = yy < content.offsetHeight - this.offsetHeight ?
+                                yy : content.offsetHeight - this.offsetHeight;
+                            $(this).css({
+                                left: xx,
+                                top: yy
+                            })
+                            return false;
                         })
-                        return false;
-                    })
-                    .bind('mouseup', function () {
-                        $(this).unbind('mousemove');
-                        return false;
-                    })
-                    .children().eq(0).dblclick(function () {
-                        $(this).parent().remove();
-                    });
-            })
-        var $dtime = $('#content').find('.dtime').eq(i + 1);
-        // 根据data数组里的毫秒值设置倒计时
-        $dtime.timer = setInterval(function () {
-            $dtime.html(fndjs(ends));
-        }, 1000);
-    })
+                        .bind('mouseup', function () {
+                            $(this).unbind('mousemove');
+                            return false;
+                        })
+                        .children().eq(0).dblclick(function () {
+                            $(this).parent().remove();
+                        });
+                })
+            clearInterval(ele.timer);
+            ele.timer = setInterval(function () {
+                $('.tip1').eq(arr[i].id-1).find('.dtime').html(fndjs(ends));
+            }, 1000);
+           
+        })
+    }
     //封装倒计时算法---------------------------------------
-
+  
     function fndjs(n) {
-
         /*两个时间相减,得到的是毫秒ms,变成秒*/
         var result = n - new Date().getTime();
         if (result > 1) {
             var second = parseInt(result / 1000 % 60); // 计算秒 ，取余  
-
+            
             var minite = parseInt(result / 1000 / 60 % 60); //计算分 ，换算有多少分，取余，余出多少秒
-
+            
             var hour = parseInt(result / 1000 / 3600 % 24); //计算小时，换算有多少小时，取余，24小时制除以24，余出多少小时
-
+            
             var day = parseInt(result / 1000 / 3600 / 24); //计算天 ，换算有多少天
-
+            
             var fdjs = '倒计时还有：' + day + "天" + hour + "小时" + minite + "分" + second + "秒";
-            return fdjs;
-        } else {
-            return;
-        }
-    };
+                return fdjs;
+            } else {
+                return;
+            }
+    };   
+    
 
     // 获取当前时间并返回------------------------------------
     function fn() {
@@ -268,5 +259,5 @@ $(function () {
     }
 
     // 将克隆源隐藏
-    $('#content').children().eq(0).hide();
+    $cont.children().eq($cont.children().length-1).hide();
 });
