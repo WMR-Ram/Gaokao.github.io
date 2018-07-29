938bf976eaef7b385e549856ca8d30203b54b7e7
sbn <sbn@.com>
2018-07-29 14:51:43 +0800

    定时器问题全部OK

--- Heart wall/js/heart.js
+++ Heart wall/js/heart.js
@@ -1,7 +1,7 @@
 $(function () {
 
 
-    // var data = ['毕业后,日薪过千', '努力让自己过的更好', '变成自己喜欢的样子', '赶紧毕业迎娶白富美', '2018新年愿望、好好锻炼 把自己练的棒棒的 身体健康 吃嘛嘛香。',
+    // var m = ['毕业后,日薪过千', '努力让自己过的更好', '变成自己喜欢的样子', '赶紧毕业迎娶白富美', '2018新年愿望、好好锻炼 把自己练的棒棒的 身体健康 吃嘛嘛香。',
     //     '2018年我的新年愿望是、这一年你可以单身，这样我就可以追你了。希望可以实现。', '人生最大的愿望就是；吃一碗广告里的泡面。', '2018年最后一个月最后一个愿望：用我身上的十斤肉换个男朋友。',
     //     '2018新年愿望、 我希望能找个女朋友……鹅，算了。还是希望世界和平吧。'
     // ]
@@ -9,16 +9,22 @@ $(function () {
 
 
     var data = [
-        {id:1,xysj: "2018-7-19 22:27:2", xy: "希望明天捡十块钱！", name:"见钱眼开", jssj:1533105211082},
-        {id:2,xysj: "2018-7-19 22:29:5", xy: "希望明天死情敌！", name:"死了都要爱", jssj:1533511211082},
-        {id:3,xysj: "2016-02-17 01:01:01", xy: "今天你拿苹果支付了么", name: "mahu", jssj:1533155211082},
-        {id:4,xysj: "2018-7-20 20:42:25", xy: "希望能找到工作", name: "梦游西天", jssj:1543103211082},
-        {id:5,xysj: "2018-7-20 20:45:19", xy: "我要500万", name: "钱眼子", jssj:1534100211082}
+        {id:1, 'bm':'n', xysj: "2018-7-19 22:27:2",   xy: "希望明天捡十块钱！",   name:"见钱眼开",    jssj:1533105211082},
+        {id:2, 'bm':'n', xysj: "2018-7-19 22:29:5",   xy: "希望明天死情敌！",     name:"死了都要爱",  jssj:1533511211082},
+        {id:3, 'bm':'n', xysj: "2016-02-17 01:01:01", xy: "今天你拿苹果支付了么", name: "mahu",       jssj:1533155211082},
+        {id:4, 'bm':'n', xysj: "2018-7-20 20:42:25",  xy: "希望能找到工作",       name: "梦游西天",   jssj:1543103211082},
+        {id:5, 'bm':'n', xysj: "2018-7-20 20:45:19",  xy: "我要500万",           name: "钱眼子",      jssj:1534100211082}
                 ];
     var $tip1 = $('.tip1');
     var $cont = $('#content');
     var ZIndex = 1;
+    
+    // 这是输入密码时遮罩层的初始层级
     var mIndex = 3;
+    var $so = $('.sosu');
+    
+    // 创建新信息时用的id；因为data中用5条数据
+    var number =  5;
     // ------------------------------------------------------
     cjfun(data);
     
@@ -56,48 +62,48 @@ $(function () {
             // 设置保密密码；
             $('.mask').show().css({
                     'zIndex': mIndex,
-                    height: $('#content')[0].offsetHeight+100,
-                })
+                    height: $('#content')[0].offsetHeight+300,
+            })
                 
-                //输入密码时的逻辑 
-                .find('.prow').keyup(function () {
-                    var str = $('.prow').val();
-                    if (str === ' ') {
-                        alert('密码不能是空格');
-                        $('.prow').val('');
-                        return;
-                    }
+            //输入密码时的逻辑 
+            .find('.prow').keyup(function () {
+                var str = $('.prow').val();
+                if (str === ' ') {
+                    alert('密码不能是空格');
+                    $('.prow').val('');
+                    return;
+                }
 
-                    //是不是用$.each循环更好？？？？？ 
-                    for (k in str) {
-                        $('.hide').text(str.charAt(str.length - 1));
-                        break;
-                    }
+                //是不是用$.each循环更好？？？？？ 
+                for (k in str) {
+                    $('.hide').text(str.charAt(str.length - 1));
+                    break;
+                }
 
-                    //定时器调用函数校对密码 
-                    var hidTimer = setTimeout(hidspan, 150);
-                }).parent('.title').children('.Ximg').click(function () {
-                    $('.mask').hide();
-                    $('.privary').children('input').removeClass('clor');
-                    $('html').css('overflow','auto');
-                }).parent('.title').children('.shuell').click(function () {
-                    $('.mask').hide();
+            //定时器调用函数校对密码 
+            var hidTimer = setTimeout(hidspan, 150);
+            }).parent('.title').children('.Ximg').click(function () {
+                $('.mask').hide();
+                $('.privary').children('input').removeClass('clor');
+                $('html').css('overflow','auto');
+            }).parent('.title').children('.shuell').click(function () {
+                $('.mask').hide();
 
-                    // 回复页面的滚动效果
-                    $('html').css('overflow','auto');
-                })
-            // ------------------------------------
+                // 回复页面的滚动效果
+                $('html').css('overflow','auto');
+            });
+        // ------------------------------------
             
             // 动态显示密码 1秒隐藏
             function hidspan() {
                 var str = $('.prow').val();
                 var sttr = $('.hide');
                 sttr.text('')
-                    .css({
-                        position: 'absolute',
-                        left: 115 + 5.7 * str.length,
-                        top: 124,
-                    });
+                .css({
+                    position: 'absolute',
+                    left: 115 + 5.7 * str.length,
+                    top: 124,
+                });
             }
         }
     });
@@ -117,6 +123,11 @@ $(function () {
         var endx = new Date(sxsjy, sxsjm, sxsjd).getTime();
         var bjendx = new Date(0, 0, 0).getTime();
         var time = new Date().getTime();
+
+        //获取content下的子元素的倒数第二个；倒数第一个是模板隐藏元素 
+        var str = $('#content').children('.tip1').eq($('#content')
+        .children('.tip1').length-2).find('.num').html();
+        str = str.substr(2,1);
 
         // 判断是否设置实现愿望的时间
         // 还应该判断最长愿望时间可以不写，但不能无限长（没写）；
@@ -138,32 +149,41 @@ $(function () {
             var priv = $('.privary .clor').attr('tada-n');
             var name = $('.names').val();
             var arr = [];
-            
+            var prow = $('.title > #prow').val();
+           
+            // 验证密码
+            var yaz  = $('').val();
+
             var bmxysz = [];
+            var json = {};
+            number++;
             if (priv === 'y') {
-                var json = {'id': data.length + 1};
+                json  = {'id': number, 'bm':priv, 'name':name};
                 var mjson = {};
                 bmxysz[bmxysz.length] = {
-                    'id': data.length + 1,
-                    'xysj': fn(),
-                    'xy': text,
-                    'xb': sex,
-                    'jssj': endx
-                };
-                
-            } else {
-                var json = {};
-                json = {
-                    'id': data.length + 1,
+                    'id': number,
+                    'bm':priv,
                     'xysj': fn(),
                     'xy': text,
                     'name': name,
                     'jssj': endx
-                }
+                };
+                mjson[prow] = bmxysz.length;
+            } else {
+                var json = {};
+                json = {
+                    'id': number,
+                    'bm':priv,
+                    'xysj': fn(),
+                    'xy': text,
+                    'name': name,
+                    'jssj': endx
+                };
             }
             data[data.length] = json;
             arr[arr.length] = json;
-            cjfun(arr);
+            console.log(data);
+            cjfun(arr,str);
         }else{
             alert('请输入心愿内容！');
         }
@@ -172,32 +192,69 @@ $(function () {
     });
     // ----------------------------------------
     
-    // 根据data数组里的每一项创建心愿卡
-    function cjfun(arr){
+    // 鼠标移入放大镜搜索框动态增长 自动获取光标
+    $('#bzd').mouseenter(function(){
+        $so.stop().animate({width:208},800)
+        .focus();
+    })
+    .click(function(){
+        var str = $('#content').children('.tip1').find('.name');
         
+        // 执行搜索心愿卡事件；
+        $(str).each(function(i,ele){
+            if($(ele).html() === $so.val()){
+                $(ele).parent().parent().stop().animate({
+                    'zIndex':ZIndex++,
+                    top:64,
+                    left:738
+                },1000);
+                $so.val('');
+            }
+        });
+    })
+    .mouseleave(function(){
+        $so.stop().animate({width:22},800);
+    });
+
+    // ----------------------------------------
+
+    // 根据data数组里的每一项创建心愿卡
+    function cjfun(arr,tr){
+        tr = tr > 0 ? tr : 0;
         //结束时间的毫秒值在data数组中获取的
         $.each(arr, function (i, ele) {
-            var ends = ele.jssj;
-            $tip1.clone(true).insertBefore($tip1)
-                .find('.num').html('第[' + ele.id + ']条' + ele.xysj)
-                .parents('.tip1').find('.tip_c').text(ele.xy)
-                .parents('.tip1').find('.name').text(ele.name)
-                .parents('.tip1').css({
-                    left: $tip1[0].offsetLeft + Math.random() * ($cont[0].offsetWidth -
-                        $tip1[0].offsetWidth - $tip1[0].offsetLeft),
-                    top: Math.random() * 400
-                }).show()
-    
-                // on()：jquery事件监听器
-                .parents('#content').on('mousedown', '.tip1', function (e) {
-                    e = e || document.event;
-                    $(this).css('zIndex', ZIndex);
-    
-                    // mIndex设置遮罩层的层级
-                    mIndex = 2 + ZIndex++;
-                    var x = e.clientX - this.offsetLeft;
-                    var y = e.clientY - this.offsetTop;
-                    $(this).bind('mousemove', function (e) {
+                 
+            if(ele.id > tr){
+                var ends = ele.jssj;
+
+                //判断许愿时间是否为空 
+                if(ele.xysj === undefined){
+                    ele.xysj = '';
+                }
+                if(ele.bm === 'y'){
+                    ele.xy = '<span class="lock"></span>';
+                    ends = fn();
+                }
+                $tip1.clone(true).insertBefore($tip1)
+                    .find('.num').html('第[' + ele.id + ']条' + ele.xysj)
+                    .parents('.tip1').find('.tip_c').html(ele.xy)
+                    .parents('.tip1').find('.name').text(ele.name)
+                    .parents('.tip1').css({
+                        left: $tip1[0].offsetLeft + Math.random() * ($cont[0].offsetWidth -
+                            $tip1[0].offsetWidth - $tip1[0].offsetLeft),
+                        top: Math.random() * 400
+                    }).show().attr('data-c',ele.id)
+        
+                    // on()：jquery事件监听器
+                    .parents('#content').on('mousedown', '.tip1', function (e) {
+                        e = e || document.event;
+                        $(this).css('zIndex', ZIndex);
+        
+                        // mIndex设置遮罩层的层级
+                        mIndex = 2 + ZIndex++;
+                        var x = e.clientX - this.offsetLeft;
+                        var y = e.clientY - this.offsetTop;
+                        $(this).bind('mousemove', function (e) {
                             e = e || document.event;
                             var xx = e.clientX - x;
                             var yy = e.clientY - y;
@@ -210,46 +267,80 @@ $(function () {
                             $(this).css({
                                 left: xx,
                                 top: yy
-                            })
+                            });
                             return false;
                         })
                         .bind('mouseup', function () {
                             $(this).unbind('mousemove');
                             return false;
                         })
-                        .children().eq(0).dblclick(function () {
-                            $(this).parent().remove();
-                        });
-                })
-            clearInterval(ele.timer);
-            ele.timer = setInterval(function () {
-                $('.tip1').eq(arr[i].id-1).find('.dtime').html(fndjs(ends));
-            }, 1000);
-           
-        })
+                    });
+                ffsl(ends,ele.id);
+            }else{
+                return;
+            }
+        });
     }
+    //删除心愿------------------------------------------- 
+    
+    var jjjj = $cont.children();
+    for(var i=0;i<jjjj.length;i++){
+        $(jjjj).eq(i).on('dblclick',function () {
+            var str = $(this).find('.num').html();
+
+            // 截取字符串
+            str = str.substr(2,1);
+            var str1 = data[data.length-1].id;
+            for(k in data){
+                if(data[k].id === +str){
+                    data.splice(k,1);
+                    cjfun(data,str1);
+                }
+            }
+            this.remove();
+        });
+    }
+    // 心愿定时器启动------------------------------------
+    
+    function ffsl(end,id){
+        var $str = $('#content').children('.tip1');
+        $str.each(function(i,ele){
+            var data_c = ele.getAttribute('data-c');
+
+            if(+data_c === id){
+                // console.log($(ele).find('.dtime'));
+                clearInterval(ele.timer);
+                ele.timer = setInterval(function () {
+                    $(ele).find('.dtime').html(fndjs(end));
+                }, 1000);
+            }
+        })
+       
+        // return 
+    }
+    
     //封装倒计时算法---------------------------------------
   
     function fndjs(n) {
-        /*两个时间相减,得到的是毫秒ms,变成秒*/
-        var result = n - new Date().getTime();
-        if (result > 1) {
-            var second = parseInt(result / 1000 % 60); // 计算秒 ，取余  
-            
-            var minite = parseInt(result / 1000 / 60 % 60); //计算分 ，换算有多少分，取余，余出多少秒
-            
-            var hour = parseInt(result / 1000 / 3600 % 24); //计算小时，换算有多少小时，取余，24小时制除以24，余出多少小时
-            
-            var day = parseInt(result / 1000 / 3600 / 24); //计算天 ，换算有多少天
-            
-            var fdjs = '倒计时还有：' + day + "天" + hour + "小时" + minite + "分" + second + "秒";
+        
+            /*两个时间相减,得到的是毫秒ms,变成秒*/
+            var result = n - new Date().getTime();
+            if (result > 1) {
+                var second = parseInt(result / 1000 % 60); // 计算秒 ，取余  
+                
+                var minite = parseInt(result / 1000 / 60 % 60); //计算分 ，换算有多少分，取余，余出多少秒
+                
+                var hour = parseInt(result / 1000 / 3600 % 24); //计算小时，换算有多少小时，取余，24小时制除以24，余出多少小时
+                
+                var day = parseInt(result / 1000 / 3600 / 24); //计算天 ，换算有多少天
+                
+                var fdjs = '倒计时还有：' + day + "天" + hour + "小时" + minite + "分" + second + "秒";
+                
                 return fdjs;
             } else {
                 return;
             }
-    };   
-    
-
+        }
     // 获取当前时间并返回------------------------------------
     function fn() {
         var time = new Date();
